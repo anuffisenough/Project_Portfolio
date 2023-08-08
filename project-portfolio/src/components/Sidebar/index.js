594d34f2ac3ffb87c3b943bd4ffb8808c708f2ed
anuffisenough <uffmanch@gmail.com>
2023-08-08 16:57:27 -0600

    updates components folder

--- project-portfolio/src/components/Sidebar/index.js
+++ project-portfolio/src/components/Sidebar/index.js
@@ -8,6 +8,29 @@ const Sidebar = () => (
             <img src={LogoS} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="subtitle" />
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/christopher-uffman-a62575130/'>
+                    <FontAwesomeIcon icon={faLinkedIn} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/anuffisenough'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
