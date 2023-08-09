b4472991c6a56421a6fa00f4f2e4161f10926976
anuffisenough <uffmanch@gmail.com>
2023-08-09 12:32:15 -0600

    updates dashboard and home files

--- project-portfolio/src/components/Contact/index.js
+++ project-portfolio/src/components/Contact/index.js
@@ -45,6 +45,61 @@ const Contact = () => {
                     </h1>
                     <p>I am interested in freelance opportunities - especially on ambitious or large projects. However, if you have any other requests or questions, don't hesitate to contact me using the form below.                        
                     </p>
-                    </div></div></>
+                    <div className="contact-form">
+                        <form ref={form} onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                    <input placeholder="Name" type="text" name="name" required />
+                                </li>
+                                <li className="half">
+                                    <input
+                                        placeholder="Email"
+                                        type="email"
+                                        name="email"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        placeholder="Subject"
+                                        type="text"
+                                        name="subject"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <textarea
+                                        placeholder="Message"
+                                        name="message"
+                                        required
+                                    ></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+                <div className="info-map">
+                    Christopher Uffman,
+                    <br />
+                    Denver, Contact
+                    <br />
+                    <span>uffmanch@gmail.com</span>
+                </div>
+                <div className="map-wrap">
+                    <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker position={[44.96366, 19.61045]}>
+                            <Popup>Christopher lives here</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+            </div>
+        <Loader type="pacman" />
+        </>
     )
 }
+
+export default Contact
