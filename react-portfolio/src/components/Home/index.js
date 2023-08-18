365f0ac9592558dc2363632fe96e27498cbbf17f
anuffisenough <uffmanch@gmail.com>
2023-08-18 14:06:29 -0600

    fixes transition bug in home page

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -24,35 +24,22 @@ const Home = () => {
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
-                <span className={letterClass}>W</span>
-                <span className={`${letterClass} _12`}>e</span>
-                <span className={`${letterClass} _13`}>l</span>
-                <span className={`${letterClass} _14`}>l</span>
-                <span> </span>
-                <span className={`${letterClass} _15`}>h</span>
-                <span className={`${letterClass} _16`}>e</span>
-                <span className={`${letterClass} _17`}>l</span>
-                <span className={`${letterClass} _18`}>l</span>
-                <span className={`${letterClass} _19`}>o </span>
-                <span> </span>
-                <span className={`${letterClass} _20`}>t</span>
-                <span className={`${letterClass} _21`}>h</span>
-                <span className={`${letterClass} _22`}>e</span>
-                <span className={`${letterClass} _23`}>r</span>
-                <span className={`${letterClass} _24`}>e</span>
-                <span className={`${letterClass} _25`}>!</span>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i</span>
+                <span className={`${letterClass} _13`}>!</span>
                 <br />
-                <span className={`${letterClass} _26`}>I</span>
-                <span className={`${letterClass} _27`}>'</span>
-                <span className={`${letterClass} _28`}>m</span>
+                <span className={`${letterClass} _14`}>I</span>
+                <span className={`${letterClass} _15`}>'</span>
+                <span className={`${letterClass} _16`}>m</span>
+                <span className={`${letterClass} _17`}> </span>
                 <img src={LogoTitle} alt="developer" />
                 <AnimatedLetters letterClass={letterClass}
                 strArray={nameArray}
-                idx={29} />
+                idx={18} />
                 <br />
                 <AnimatedLetters letterClass={letterClass}
                 strArray={jobArray}
-                idx={30} />
+                idx={16} />
                 </h1>
                 <h2>Full Stack Developer</h2>
                 <Link to="/contact" className="flat-button">Contact Me</Link>
